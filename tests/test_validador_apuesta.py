25067c0b41fac5efbd472004102b88df382fa600
AndresChCy <aichaparrom@gmail.com>
2025-09-01 12:25:52 -0400

    RED: Test para validar rangos de la pinta y cantidades

--- tests/test_validador_apuesta.py
+++ tests/test_validador_apuesta.py
@@ -86,3 +86,50 @@ def test_cambiar_apuesta_de_as_invalida():
     assert validador.pinta_actual == 1
     assert validador.cantidad_actual == cantidad
     assert "Cantidad invalida para cambio de as" in str(exc_info.value)
+
+def test_empezar_apuesta_de_pinta_fuera_de_rango():
+    pinta = 0
+    cantidad = 2
+    with pytest.raises(ValueError) as exc_info:
+        validador = ValidadorApuesta(pinta,cantidad)
+    assert "Pinta fuera de rango" in str(exc_info.value)
+
+def test_empezar_apuesta_de_pinta_fuera_de_rango2():
+    pinta = 7
+    cantidad = 2
+    with pytest.raises(ValueError) as exc_info:
+        validador = ValidadorApuesta(pinta,cantidad)
+    assert "Pinta fuera de rango" in str(exc_info.value)
+
+def test_empezar_apuesta_con_cantidad_negativa():
+    pinta = 2
+    cantidad = -1
+    with pytest.raises(ValueError) as exc_info:
+        validador = ValidadorApuesta(pinta,cantidad)
+    assert "Cantidad fuera de rango" in str(exc_info.value)
+
+def test_apostar_con_pinta_fuera_de_rango():
+    pinta = 0
+    cantidad = 3
+    validador = ValidadorApuesta(2,2)
+    with pytest.raises(ValueError) as exc_info:
+        validador.apostar(pinta,cantidad)
+    assert validador.cantidad_actual == 2
+    assert validador.pinta_actual == 2
+
+def test_apostar_con_pinta_fuera_de_rango2():
+    pinta = 7
+    cantidad = 3
+    validador = ValidadorApuesta(2,2)
+    with pytest.raises(ValueError) as exc_info:
+        validador.apostar(pinta,cantidad)
+    assert "Pinta fuera de rango" in str(exc_info.value)
+    assert validador.cantidad_actual == 2
+    assert validador.pinta_actual == 2
+
+def test_apostar_con_cantidad_fuera_de_rango():
+    validador = ValidadorApuesta(2,2)
+    with pytest.raises(ValueError) as exc_info:
+        validador.apostar(2,-1)
+    assert validador.cantidad_actual == 2
+    assert validador.pinta_actual == 2
