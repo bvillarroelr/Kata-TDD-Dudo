9a327d3783fcad823c0770a06659d461891bd58f
AndresChCy <aichaparrom@gmail.com>
2025-09-01 17:14:19 -0400

    RED: Test para construir el gestor de partida

---
+++ tests/test_gestor_partida.py
@@ -0,0 +1,8 @@
+import pytest
+
+
+def test_base_partida():
+    jugadores = ["Andrés", "Benjamín","Alex"]
+    gestor = GestorPartida(jugadores)
+    for jugador in jugadores:
+        assert gestor.cachos[jugador].getCantidadDados() == 5
