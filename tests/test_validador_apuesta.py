3e1a7a17413c9433272743f0eb6135acf51c94ec
AndresChCy <aichaparrom@gmail.com>
2025-08-31 21:45:31 -0400

    RED: Test de comportamiento cuando se intenta empezar apuesta con la pinta as

--- tests/test_validador_apuesta.py
+++ tests/test_validador_apuesta.py
@@ -1,8 +1,17 @@
 import pytest
 from validador_apuesta import *
 
+
+
 def test_empezar_apuesta():
     pinta = 2
     cantidad = 3
-    validador = ValidadorApuesta(2,3)
+    validador = ValidadorApuesta(pinta,cantidad)
     assert validador is not None
+
+def test_empezar_apuesta_con_as():
+    pinta = 1
+    cantidad = 2
+    with pytest.raises(ValueError) as exc_info:
+        validador = ValidadorApuesta(pinta,cantidad)
+    assert "No puedes empezar la apuesta con un as" in str(exc_info.value)
