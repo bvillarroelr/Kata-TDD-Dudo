f4c58625ba52cbae2a1bb1fb1a5ad5bd12d029ca
AndresChCy <aichaparrom@gmail.com>
2025-09-01 14:46:11 -0400

    RED: Test para contar las pintas de los cachos con la nueva clase

---
+++ tests/test_contador_de_pintas.py
@@ -0,0 +1,19 @@
+import pytest
+
+
+def test_contador(mocker):
+    # Crear mocks de Cacho usando mocker
+    cacho1 = mocker.Mock()
+    cacho1.getResultados.return_value = [1, 2, 2, 3, 6]
+
+    cacho2 = mocker.Mock()
+    cacho2.getResultados.return_value = [1, 2, 3, 4, 5]
+
+    contador = ContadorDePintas([cacho1, cacho2])
+
+    resultado = contador.contar()
+    trenes = contador.contar(pinta=3)
+    assert resultado == [2, 5, 4, 3, 3, 3]
+
+    cacho1.getResultados.assert_called_once()
+    cacho2.getResultados.assert_called_once()
