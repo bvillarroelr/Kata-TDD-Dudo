4cb22f04a1aa1a9164bbba9176ba6b90dc458c99
AndresChCy <aichaparrom@gmail.com>
2025-08-31 23:01:28 -0400

    GREEN: test para impedir empezaar con as ahora pasa correctamente

--- src/validador_apuesta.py
+++ src/validador_apuesta.py
@@ -3,5 +3,7 @@
 class ValidadorApuesta:
 
     def __init__(self,pinta,cantidad):
+        if pinta == 1:
+            raise ValueError("No puedes empezar la apuesta con un as")
         self.pinta_actual = pinta
         self.cantidad = cantidad
