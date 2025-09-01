f3e78b3a7b9cca82fc21cf70dc6018e76222590e
AndresChCy <aichaparrom@gmail.com>
2025-09-01 12:36:31 -0400

    GREEN: Se implementan metodos para verificar los rangos adecuados y se asigna al constructor tanto como la funcion de apostar

--- src/validador_apuesta.py
+++ src/validador_apuesta.py
@@ -6,8 +6,7 @@ class ValidadorApuesta:
     def __init__(self,pinta,cantidad):
         if pinta == 1:
             raise ValueError("No puedes empezar la apuesta con un as")
-        self.pinta_actual = pinta
-        self.cantidad_actual = cantidad
+        self._actualizar_apuesta(pinta,cantidad)
 
     def apostar(self, pinta, cantidad):
         if self._es_cambio_a_as(pinta, cantidad):
@@ -37,7 +36,19 @@ class ValidadorApuesta:
         if self.pinta_actual == 1 and self.cantidad_actual * 2 + 1 != cantidad:
             raise ValueError("Cantidad invalida para cambio de as")
 
+    def _validar_rango_pinta(self,pinta):
+        if pinta < 1  or pinta > 6:
+            raise ValueError("Pinta fuera de rango")
+
+    def _validar_rango_cantidad(self,cantidad):
+        if cantidad < 1:
+            raise ValueError("Cantidad fuera de rango")
+
     def _actualizar_apuesta(self, pinta, cantidad):
+        self._validar_rango_pinta(pinta)
+        self._validar_rango_cantidad(cantidad)
         self.pinta_actual = pinta
         self.cantidad_actual = cantidad
         return True
+
+
