17ac5438c0581f7e4d4fe531d79b9cf1b1df3003
AndresChCy <aichaparrom@gmail.com>
2025-09-01 14:58:16 -0400

    RED: Test para rondas especiales donde solo queda un dado y los ases no son comodin

--- tests/test_contador_de_pintas.py
+++ tests/test_contador_de_pintas.py
@@ -4,7 +4,6 @@ from contador_de_pintas import ContadorDePintas
 
 
 def test_contador(mocker):
-    # Crear mocks de Cacho usando mocker
     cacho1 = mocker.Mock()
     cacho1.getResultados.return_value = [1, 2, 2, 3, 6]
 
@@ -17,6 +16,23 @@ def test_contador(mocker):
     trenes = contador.contar(pinta=3)
     assert resultado == [2, 5, 4, 3, 3, 3]
     assert trenes == 4
+
+    cacho1.getResultados.assert_called_once()
+    cacho2.getResultados.assert_called_once()
+
+def test_contador_ronda_de_un_dado(mocker):
+    cacho1 = mocker.Mock()
+    cacho1.getResultados.return_value = [1, 2, 2, 3, 6]
+
+    cacho2 = mocker.Mock()
+    cacho2.getResultados.return_value = [1, 2, 3, 4, 5]
+
+    contador = ContadorDePintas([cacho1, cacho2])
+
+    resultado = contador.contar(un_dado=True)
+    trenes = contador.contar(pinta=3, un_dado=True)
+    assert resultado == [2, 3, 2, 1, 1, 1]
+    assert trenes == 2
 
     cacho1.getResultados.assert_called_once()
     cacho2.getResultados.assert_called_once()
