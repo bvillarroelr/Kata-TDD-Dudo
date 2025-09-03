d8c0f82ca537ed78f34f0c7fb4de117ba8bc37d9
AndresChCy <aichaparrom@gmail.com>
2025-09-02 22:49:36 -0400

    RED: Test de integracion probando funcionalidad al dudar en juego

--- tests/test_gestor_partida.py
+++ tests/test_gestor_partida.py
@@ -36,5 +36,19 @@ def test_rotacion_correcta(mocker):
     gestor.jugar()
     assert gestor.jugador_en_turno() == "Alex"
 
-
-
+def test_jugador_duda_mal(mocker):
+    jugadores = ["Andrés", "Benjamín", "Alex"]
+    valores_dados = [
+        6, 2, 4,  # Para que Andrés empiece
+        5, 5, 5, 5, 5,  # Andrés
+        5, 5, 5, 5, 5,  # Benjamín
+        5, 5, 5, 5, 5  # Alex
+    ]
+    mocker.patch("random.randint", side_effect=valores_dados)
+    mocker.patch('builtins.input', side_effect=["derecha", 5, 3, "dudar"])
+    gestor = GestorPartida(jugadores)
+    gestor.empezar_turno()
+    gestor.jugar()
+    assert gestor.cachos["Benjamín"].getCantidadDados() == 4
+    assert gestor.cachos["Andrés"].getCantidadDados() == 5
+    assert gestor.cachos["Andrés"].buffer == 1
