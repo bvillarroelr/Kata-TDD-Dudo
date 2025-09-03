cf4c81f100e717a229e77921eb14145bbd9f960b
AndresChCy <aichaparrom@gmail.com>
2025-09-02 23:27:38 -0400

    RED: Tests para analizar casos de duda correcta y ademas casos al calzar

--- tests/test_gestor_partida.py
+++ tests/test_gestor_partida.py
@@ -52,3 +52,54 @@ def test_jugador_duda_mal(mocker):
     assert gestor.cachos["Benjamín"].getCantidadDados() == 4
     assert gestor.cachos["Andrés"].getCantidadDados() == 5
     assert gestor.cachos["Andrés"].buffer == 1
+
+def test_jugador_duda_bien(mocker):
+    jugadores = ["Andrés", "Benjamín", "Alex"]
+    valores_dados = [
+        6, 2, 4,  # Para que Andrés empiece
+        5, 5, 5, 5, 5,  # Andrés
+        5, 5, 5, 5, 5,  # Benjamín
+        5, 5, 5, 5, 5  # Alex
+    ]
+    mocker.patch("random.randint", side_effect=valores_dados)
+    mocker.patch('builtins.input', side_effect=["derecha", 3, 3, "dudar"])
+    gestor = GestorPartida(jugadores)
+    gestor.empezar_turno()
+    gestor.jugar()
+    assert gestor.cachos["Benjamín"].getCantidadDados() == 5
+    assert gestor.cachos["Andrés"].getCantidadDados() == 4
+    assert gestor.cachos["Benjamín"].buffer == 1
+
+def test_jugador_calza_bien(mocker):
+    jugadores = ["Andrés", "Benjamín", "Alex"]
+    valores_dados = [
+        6, 2, 4,  # Para que Andrés empiece
+        5, 5, 5, 5, 5,  # Andrés
+        5, 5, 5, 5, 5,  # Benjamín
+        5, 5, 5, 5, 5  # Alex
+    ]
+    mocker.patch("random.randint", side_effect=valores_dados)
+    mocker.patch('builtins.input', side_effect=["derecha", 5, 15, "calzar"])
+    gestor = GestorPartida(jugadores)
+    gestor.empezar_turno()
+    gestor.jugar()
+    assert gestor.cachos["Benjamín"].getCantidadDados() == 5
+    assert gestor.cachos["Andrés"].getCantidadDados() == 5
+    assert gestor.cachos["Benjamín"].buffer == 1
+
+def test_jugador_calza_mal(mocker):
+    jugadores = ["Andrés", "Benjamín", "Alex"]
+    valores_dados = [
+        6, 2, 4,  # Para que Andrés empiece
+        5, 5, 5, 5, 5,  # Andrés
+        5, 5, 5, 5, 5,  # Benjamín
+        5, 5, 5, 5, 5  # Alex
+    ]
+    mocker.patch("random.randint", side_effect=valores_dados)
+    mocker.patch('builtins.input', side_effect=["derecha", 3, 25, "calzar"])
+    gestor = GestorPartida(jugadores)
+    gestor.empezar_turno()
+    gestor.jugar()
+    assert gestor.cachos["Benjamín"].getCantidadDados() == 4
+    assert gestor.cachos["Andrés"].getCantidadDados() == 5
+    assert gestor.cachos["Andrés"].buffer == 0
